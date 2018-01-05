573ea0bd459e6c93609bfc1235e33aa97013fcbb
Guillaume Leroi <leroi.g@gmail.com>
2018-01-06 00:33:26 +0100

    anim and movement computed in function of elapsed time

--- game.go
+++ game.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"os"
 	"time"
@@ -19,27 +20,31 @@ func main() {
 type Direction int
 
 const (
-	DirR Direction = 0
-	DirD Direction = 1
-	DirL Direction = 2
-	DirU Direction = 3
+	DirR Direction = iota
+	DirD
+	DirL
+	DirU
 )
 
 type player struct {
 	picture   pixel.Picture
 	anim      int
+	animStep  float64
 	direction Direction
 	position  pixel.Vec
 	sprite    *pixel.Sprite
 }
 
+const spriteCols = 9
+const spriteRows = 4
+
 func (p *player) Draw(target pixel.Target) {
-	const spriteCols = 9
-	const spriteRows = 4
 	spriteWidth := p.picture.Bounds().Max.X / spriteCols
 	spriteHeight := p.picture.Bounds().Max.Y / spriteRows
-	spriteX := float64(1+p.anim%(spriteCols-1)) * spriteWidth
-	spriteY := float64(p.anim/spriteCols) * spriteHeight
+
+	animOffset := int(p.direction)*spriteCols + p.anim
+	spriteX := float64(animOffset%spriteCols) * spriteWidth
+	spriteY := float64(animOffset/spriteCols) * spriteHeight
 
 	bounds := pixel.R(spriteX, spriteY, spriteX+spriteWidth, spriteY+spriteHeight)
 	if p.sprite == nil {
@@ -74,46 +79,67 @@ func run() {
 	}
 
 	last := time.Now()
+	animInterval := 4.5
+	speed := 76.0
 	for !win.Closed() {
 		now := time.Now()
 		elapsed := now.Sub(last)
 
+		deltaM := 1.0
+		var move pixel.Vec
 		if win.Pressed(pixelgl.KeyLeft) {
 			p.direction = DirL
+			move = pixel.V(-deltaM, 0)
 		}
 		if win.Pressed(pixelgl.KeyRight) {
 			p.direction = DirR
+			move = pixel.V(deltaM, 0)
 		}
 		if win.Pressed(pixelgl.KeyDown) {
 			p.direction = DirD
+			move = pixel.V(0, -deltaM)
 		}
 		if win.Pressed(pixelgl.KeyUp) {
 			p.direction = DirU
+			move = pixel.V(0, deltaM)
 		}
 
-		if elapsed >= 55*time.Millisecond {
-			p.anim = int(p.direction)*9 + (p.anim+1)%9
+		if win.JustPressed(pixelgl.KeyV) {
+			animInterval += 0.5
+			fmt.Printf("animInterval %f\n", animInterval)
+		}
+		if win.JustPressed(pixelgl.KeyC) {
+			animInterval -= 0.5
+			fmt.Printf("animInterval %f\n", animInterval)
+		}
 
-			var move pixel.Vec
-			const deltaM = 5.0
-			switch p.direction {
-			case DirR:
-				move = pixel.V(deltaM, 0)
-			case DirD:
-				move = pixel.V(0, -deltaM)
-			case DirL:
-				move = pixel.V(-deltaM, 0)
-			case DirU:
-				move = pixel.V(0, deltaM)
+		if win.JustPressed(pixelgl.KeyF) {
+			speed += 0.5
+			fmt.Printf("speed %f\n", speed)
+		}
+		if win.JustPressed(pixelgl.KeyD) {
+			speed -= 0.5
+			fmt.Printf("speed %f\n", speed)
+		}
+
+		if move.Len() > 0.0 {
+			distance := elapsed.Seconds() * speed
+			p.position = p.position.Add(move.Scaled(distance))
+			p.animStep += distance
+			if p.animStep > animInterval {
+				p.anim = 1 + p.anim%(spriteCols-1)
+				p.animStep = 0
 			}
-			p.position = p.position.Add(move)
-
-			last = now
+		} else {
+			p.anim = 0
+			p.animStep = 0
 		}
 
 		win.Clear(colornames.Burlywood)
 		p.Draw(win)
 		win.Update()
+
+		last = now
 	}
 }
 
